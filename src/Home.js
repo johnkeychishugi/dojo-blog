865f0c811d7ea652f8e9720bcd8e694ca866e5bf
John Chishugi <jkchishugi@gmail.com>
2021-10-15 21:05:59 +0100

    ft-add-bouton

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react"; 
+
 const Home = () => {
+    // let name = 'Mario';
+    const [name,setName] = useState('Mario');
+    const [age,setAge] = useState(24);
+
+    const handleClick = () =>{
+        setName('John');
+        setAge(29);
+    }
+
     return ( 
         <div className="home">
             <h1>Home Page</h1>
+            <p>{ name }</p>
+            <p>{ age }</p>
+            <button onClick={ handleClick }>Click Me</button>
         </div>
      );
 }
