d04294f738f3b949732767e47854890b4a1fa3d1
John Chishugi <jkchishugi@gmail.com>
2021-10-15 23:07:41 +0100

    ft-fetch-api

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,5 @@
 const BlogList = (props) => {
-     const { blogs, title, handleDelete } =  props;
+     const { blogs, title} =  props;
 
     return ( 
         <div className="blog-list">
@@ -8,7 +8,6 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id) }>delete blog</button>
                 </div> 
            ))}
         </div>
