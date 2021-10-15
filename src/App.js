8b5e88b152bc00f4083f1aaccd503b676d28832b
John Chishugi <jkchishugi@gmail.com>
2021-10-15 20:21:47 +0100

    ft-add-home-component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog'; 
-  const likes = 50;
-  // const person = {name : 'John', age:24};
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person.name }</p> */}
-        <p>{ 10 }</p>
-        <p>{ "Hello John" }</p>
-        <p>{ [1,2,3,5,5,6,] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={ link } >Google site</a>
+        <Home />
       </div> 
     </div>
   );
