aba4500a38732626750660f487bae4f5a60b1df3
John Chishugi <jkchishugi@gmail.com>
2021-10-15 20:10:08 +0100

    ft-handle-component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog'; 
+  const likes = 50;
+  // const person = {name : 'John', age:24};
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+
+        {/* <p>{ person.name }</p> */}
+        <p>{ 10 }</p>
+        <p>{ "Hello John" }</p>
+        <p>{ [1,2,3,5,5,6,] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link } >Google site</a>
+      </div> 
     </div>
   );
 }
